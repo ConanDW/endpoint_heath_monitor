a235ea44c2945ad257ac57163e5487bfa56072d7
Cameron Day <32853335+ConanDW@users.noreply.github.com>
2024-11-19 20:12:00 -0500

    Update healthcheckscript.py
    - Added check for free ram.
    - Added a print statement to show free disk space and free ram.

--- healthcheckscript.py
+++ healthcheckscript.py
@@ -11,14 +11,20 @@ def check_reboot():
     
 def check_disk_usage(disk):
     """Returns true if there isn't enough disk space, False otherwise."""
+    min_percent = 10
+    min_gb = 10
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free / du.total
     gigabytes_free = du.free / 2**30
     if percent_free < min_percent or gigabytes_free < min_gb:
-        return True
+        return True, percent_free
     else:
         return False
-    
+
+def check_free_ram():
+    free_ram = psutil.virtual_memory
+    return free_ram
+
 def check_no_network():
     """Returns True if it fails to resolve Google's URL, False otherwise."""
     try:
@@ -34,12 +40,14 @@ def check_cpu_constrained():
 def main():
     checks = [
         (check_reboot, "Pending Reboot."),
-        (check_root_full, "Root partition full."),
+        (check_disk_usage("/"), "Disk is getting full."),
         (check_no_network, "No working network."),
         (check_cpu_constrained, "CPU load too high.")
     ]
+    percent_free = check_disk_usage("/")
+    free_ram = check_free_ram()
     everything_ok=True
-    for check, msg in checks():
+    for check, msg in checks:
         if check():
           print(msg)
           everything_ok = False
@@ -48,7 +56,8 @@ def main():
         sys.exit(1)
     
     print("Everything ok.")
+    print("Free Ram: {}, Free Disk Space (%): {}".format(free_ram, percent_free))
     sys.exit(0)
     
-    
+
 main()
